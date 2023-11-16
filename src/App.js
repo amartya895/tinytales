00190d4f80fb1fb8fe5fd7a83c37b630bece330d
Amartya Sen <amartyasen895@gmail.com>
2023-11-16 15:45:11 +0530

    added border

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <div className="p-2 text-blue-500 inline-block mt-4 rounded-tr-md font-semibold text-lg border-t-2 border-r-2">
         134 Posts
       </div>
-      <div className="w-[73%] bg-gray-400 border-b-2 ml-[100px]"></div>
+      <div className="w-[74%] bg-gray-400 border-b-2 ml-[100px]"></div>
       <PostCard/>
     </div>
   );
