74c0572f4245257fba76adc84532af72cb78c450
Amartya Sen <amartyasen895@gmail.com>
2023-11-16 15:43:41 +0530

    added post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Features from "./components/Features";
 import Navbar from "./components/Navbar";
+import PostCard from "./components/PostCard";
 import Profile from "./components/Profile";
 
 function App() {
@@ -10,7 +11,11 @@ function App() {
       <div className="ml-8">
         <Features />
       </div>
-      
+      <div className="p-2 text-blue-500 inline-block mt-4 rounded-tr-md font-semibold text-lg border-t-2 border-r-2">
+        134 Posts
+      </div>
+      <div className="w-[73%] bg-gray-400 border-b-2 ml-[100px]"></div>
+      <PostCard/>
     </div>
   );
 }
